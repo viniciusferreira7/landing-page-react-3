f138576a9b66ef74f563054fcf3c86e6a56137ac
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-04-18 19:20:01 -0300

    mains informações sobre o styled-components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './styles/styles';
 
 import Home from './templetes/App';
 
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>,
 );
