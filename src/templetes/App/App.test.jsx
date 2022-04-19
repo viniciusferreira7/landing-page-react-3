62e895a5dbaa284138dcb97cbd10237c94492a67
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-04-19 20:37:23 -0300

    realizando testes

--- src/templetes/App/App.test.jsx
+++ src/templetes/App/App.test.jsx
@@ -1,6 +1,9 @@
 import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
 
 test('renders learn react link', () => {
   renderTheme(<Home />);
+  const headingContainer = screen.getbyRole('heading', { name: 'Hello' }).parentElement;
+  expect(headingContainer).toHaveStyle(`background: ${theme.colors.secondaryBg}`);
 });
