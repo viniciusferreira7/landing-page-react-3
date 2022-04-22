5be4367b62b73f1ce2c553cb9599253cc68e408d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-04-21 21:55:22 -0300

    finalinzado a instalação do jest-styled-components

--- src/templetes/App/App.test.jsx
+++ src/templetes/App/App.test.jsx
@@ -1,11 +1,10 @@
 import { screen } from '@testing-library/react';
 import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
 
 test('renders learn react link', () => {
   renderTheme(<Home />);
   const headingContainer = screen.getByRole('heading', { name: 'Hello' }).parentElement;
-  expect(headingContainer).toHaveStyle({ background: theme.colors.mainBg });
+  expect(headingContainer).toHaveStyleRule('background', 'darkred');
   expect(headingContainer).toMatchSnapshot();
 });
