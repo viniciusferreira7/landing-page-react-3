79d0501ad883973597d8a5a93b749a6e2ee68ccb
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-05-03 22:20:01 -0300

    fazendo os teste da propriedade do heading

--- src/components/Heading/Heading.test.jsx
+++ src/components/Heading/Heading.test.jsx
@@ -1,9 +1,35 @@
+import { screen } from '@testing-library/react';
 import { Heading } from '.';
 import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
 
 describe('<Heading/>', () => {
   it('should render with default values', () => {
-    const { debug } = renderTheme(<Heading>Texto</Heading>);
-    debug();
+    renderTheme(<Heading>Texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'Texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.primaryColor,
+      'font-size': theme.font.sizes.xlarge,
+      'text-transform': 'none',
+    });
+  });
+
+  it('should render with white color', () => {
+    renderTheme(<Heading colorDark>Texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'Texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.white,
+    });
+  });
+
+  it('should render with correct heading sizes', () => {
+    renderTheme(<Heading size="small">Texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'Texto' });
+
+    expect(heading).toHaveStyle({
+      'font-size': theme.font.sizes.medium,
+    });
   });
 });
