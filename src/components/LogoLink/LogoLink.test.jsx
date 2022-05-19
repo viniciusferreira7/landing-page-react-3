1877e7fed8e354cc36c4df4322e8c96e9d8bae5c
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-05-19 20:30:49 -0300

    finalizando os teste do LogoLink

--- src/components/LogoLink/LogoLink.test.jsx
+++ src/components/LogoLink/LogoLink.test.jsx
@@ -3,8 +3,28 @@ import { LogoLink } from '.';
 import { renderTheme } from '../../styles/render-theme';
 
 describe('<LogoLink/>', () => {
-  it('should render with default values', () => {
-    renderTheme(<LogoLink>Texto</LogoLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render text logo', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Ola mundo">
+        Texto
+      </LogoLink>,
+    );
+
+    const heading = screen.getByRole('heading', { name: 'Ola mundo' });
+    screen.debug();
+    expect(heading.firstChild).toHaveAttribute('href', '#target');
+  });
+
+  it('should render image logo', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Ola mundo" srcImage="image.gif">
+        Texto
+      </LogoLink>,
+    );
+
+    expect(screen.getByRole('img', { name: /Ola/i })).toHaveAttribute(
+      'src',
+      'image.gif',
+    );
   });
 });
--- src/components/LogoLink/styles.js
+++ src/components/LogoLink/styles.js
@@ -2,8 +2,10 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.a`
 ${() => css`
+  display: flex;
   text-decoration:none;
   color:inherit;
+  align-items: center;
 
   > img{
     max-height: 3rem;
