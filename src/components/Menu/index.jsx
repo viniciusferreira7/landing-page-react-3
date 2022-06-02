52a5cdf0e161b023243220f2022ca564cc52dbd4
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-01 22:13:39 -0300

    fazendo a logica de mudar o icone CloseIcon para MenuIcon

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,19 +1,20 @@
 import P from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './styles';
 import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 import { SectionContainer } from '../SectionContainer';
 import { LogoLink } from '../LogoLink';
 import { NavLinks } from '../NavLinks';
 
 export const Menu = ({ links = [], logoData }) => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <>
-      <Styled.Button>
-        <MenuIcon />
-      </Styled.Button>
-      <Styled.Container>
+      <Styled.Button>{visible ? <CloseIcon /> : <MenuIcon />}</Styled.Button>
+      <Styled.Container visible={visible}>
         <SectionContainer>
           <Styled.MenuContainer>
             <LogoLink {...logoData} />
