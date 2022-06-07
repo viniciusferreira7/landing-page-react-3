a35cabcd129d4892e3f7850b2a3cf93fe1d4bdbc
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-07 20:35:14 -0300

    aplicando estilos globais nas tags ul, ol e a

--- src/components/GridContent/styles.js
+++ src/components/GridContent/styles.js
@@ -1,5 +1,15 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-${() => css``}
+${() => css`
+  text-align:center;
+  max-width:58rem;
+  margin: 0 auto;
+`}
+`;
+
+export const Html = styled.div`
+  ${({ theme }) => css`
+  margin:${theme.spacings.xhuge} 0;
+  `}
 `;
--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -20,4 +20,16 @@ body{
 
 h1, h2, h3, h4, h5, h6{
   font-family: 'Montserrat', sans-serif;
-}`;
+  margin: ${({ theme }) => theme.spacings.large};
+}
+
+p{
+  margin: ${({ theme }) => theme.spacings.medium} 0;
+}
+
+ul, ol {
+  margin: ${({ theme }) => theme.spacings.medium};
+  padding: ${({ theme }) => theme.spacings.medium};
+}
+
+`;
