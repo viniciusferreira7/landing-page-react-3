e13544fc599a791c3f0b63f98d2911f3ca0bb672
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-09 20:22:06 -0300

    apredendo a usar a propriedades do counter do css

--- src/components/GridSection/styles.js
+++ src/components/GridSection/styles.js
@@ -1,13 +1,27 @@
 import styled, { css } from 'styled-components';
-
+import { Heading as HeadingContainer } from '../Heading';
 export const Container = styled.div`
 ${() => css``}
 `;
 
 export const Grid = styled.div`
-${() => css``}
+${() => css`
+  counter-reset: grid-counter;
+`}
 `;
 
 export const GridElement = styled.div`
-${() => css``}
+${() => css`
+  ${HeadingContainer}{
+    position: relative;
+    left:5rem;
+  }
+
+  ${HeadingContainer}::before{
+    counter-increment: grid-counter;
+    content: counter(grid-counter);
+    position: absolute;
+
+  }
+`}
 `;
