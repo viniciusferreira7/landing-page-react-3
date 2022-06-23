c7f25c5e4dabda13a1b25baaaf4bbfe43bdc75f7
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-22 21:30:06 -0300

    falha ao exbir o <Footer /> no <Base />

--- src/templetes/Base/index.jsx
+++ src/templetes/Base/index.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import * as Styled from './styles';
 import { Menu } from '../../components/Menu';
 import { Footer } from '../../components/Footer';
+import { GoTop } from '../../components/GoTop';
 
 export const Base = ({ links = [], logoData, footerHtml, children }) => {
   return (
     <>
       <Menu links={links} logoData={logoData} />
+      <GoTop />
       <Styled.Container>{children}</Styled.Container>
       <Footer html={footerHtml} />
     </>
--- src/templetes/Base/stories.jsx
+++ src/templetes/Base/stories.jsx
@@ -7,6 +7,7 @@ export default {
   args: {
     ...mock,
     children: 'Base',
+    footerHtml: '<p>Teste do Footer</p>',
   },
 };
 
